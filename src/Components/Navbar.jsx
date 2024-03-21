ff53c95d15817b11b4608c5d8e5ee6efa6cee7ac
sonnie36 <sonniemwati36@gmail.com>
2024-03-21 06:31:14 -0700

    Made the navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
 import './Navbar.css'
 import { Link } from 'react-router-dom';
+import menu from '../assets/Menu.png'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
   return (
     
         <div className="nav">
             <p className='logo'>Salome <span className='nameLogo'>Mwati</span></p>
-        <div>
+        <div className='deskTopMenu'>
           <ul className="">
             <li><Link to="/" className='navName'>Home</Link></li>
             <li><Link to="/about" className='navName'>About</Link></li>
             <li><Link to="/skills" className='navName'>Skills</Link></li>
             <li><Link to="/portfolio" className='navName'>Portfolio</Link></li>
             <li><Link to="/contact" className='navName'>Contact</Link></li>
+          </ul>     
+        </div>
+
+        {/* <div className='mobMenu'>               */}
+          <img src={menu} alt="" className='imgMenu' onClick={()=>setShowLinks(!showLinks)}/>
+        {/* <div className="menuBar"  style={{display: showLinks? 'flex': 'none'}}> */}
+          <ul className='menuBar' style={{display: showLinks? 'flex': 'none'}}>
+            <li><Link to="/" className='navMenuLinks' onClick={()=>setShowLinks(false)}>Home</Link></li>
+            <li><Link to="/about" className='navMenuLinks' onClick={()=>setShowLinks(false)}>About</Link></li>
+            <li><Link to="/skills" className='navMenuLinks' onClick={()=>setShowLinks(false)}>Skills</Link></li>
+            <li><Link to="/portfolio" className='navMenuLinks' onClick={()=>setShowLinks(false)}>Portfolio</Link></li>
+            <li><Link to="/contact" className='navMenuLinks' onClick={()=>setShowLinks(false)}>Contact</Link></li>
           </ul>
-            
-             
         </div>
-         
-        </div>
+        // </div>  
+        // </div>
   )
 }
 
